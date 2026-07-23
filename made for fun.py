a0f9331a7297f1c78125b121af4db6f5272c6792
Raunak10701 <raunaktripathi107@gmail.com>
2026-07-23 20:34:41 +0530

    Add files via upload

---
+++ made for fun.py
@@ -0,0 +1,9 @@
+while True:
+ a=eval(input("enter number"))
+ if a<100:
+  while a<100:
+    print ("python programming")
+    a = a+100
+ else:
+    print("tu thoda sa bkl hai kya. padh raha hai ki a<100 fir bhi grandmasti dikha raha hai")
+
